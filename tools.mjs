672f1287e49d0c94a1ba4e87fa8d60e950680f5b
tharnish-ship <74219950+tharnish-ship@users.noreply.github.com>
2022-03-29 10:38:37 -0400

    Uploaded some JavaScript files.

---
+++ tools.mjs
@@ -0,0 +1,20 @@
+import * as 'cards' from 'cards.js';
+let string = '';
+let playerCards = [];
+let CPUCards = [];
+function randomDraw(){
+for(let i =0; i < 7; i++){
+let randomDraw = Math.floor(Math.random() * cards.length);
+CPUCards.push(cards[randomDraw]);
+playerCards.push(cards[randomDraw+1]);
+}
+return CPUCards;
+}
+function showCards(){
+let listOfPlayerCards = randomDraw();
+listOfPlayerCards.forEach(function(element){
+string += element.name += '\n';
+});
+console.log('You have the following cards: ' + string);
+}
+showCards();
